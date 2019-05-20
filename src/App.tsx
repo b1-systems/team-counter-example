849638811c87d686a59e612c5b9bf6a79608e263
Christian Tschub <tschub@b1-systems.de>
2019-05-20 15:20:07 +0200

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//Import the TeamCounter components from npm packages
+import TeamMemberCounter from 'team-member-counter-test/dist/TeamMemberCounter'
+import TeamOwnerCounter from 'team-owner-counter-test/dist/TeamOwnerCounter'
+
+interface IProps {
+}
+
+interface IState {
+  memberCnt: number;
+  ownerCnt: number;
+}
+
+class ShowCase extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = { memberCnt:0, ownerCnt:0 };
+  }
+
+  onIncreaseOwner = () => this.setState({ ownerCnt: this.state.ownerCnt + 1 });
+  onIncreaseMember = () => this.setState({memberCnt: this.state.memberCnt + 1});
+
+  //Use the TeamCounter components in the render function
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncreaseMember}>+</button>
+        <TeamMemberCounter count={this.state.memberCnt} />
+        <button onClick={this.onIncreaseOwner}>+</button>
+        <TeamOwnerCounter count={this.state.ownerCnt}/>
+      </div>
+    );
+  }
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ShowCase/>
       </header>
     </div>
   );
