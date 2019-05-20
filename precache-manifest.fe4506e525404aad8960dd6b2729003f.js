2cf1f464aaa826e057a08a13ff1e9e4e61107cc0
Christian Tschub <tschub@b1-systems.de>
2019-05-20 15:32:55 +0200

    Updates

---
+++ precache-manifest.fe4506e525404aad8960dd6b2729003f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7120f1e03c4182cdfb30b2a9182dc2",
+    "url": "/team-counter-example/index.html"
+  },
+  {
+    "revision": "a4815cc3789f1e3d28ee",
+    "url": "/team-counter-example/static/css/2.34dc3d13.chunk.css"
+  },
+  {
+    "revision": "c76f3edaa3b5d49e4bc3",
+    "url": "/team-counter-example/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "a4815cc3789f1e3d28ee",
+    "url": "/team-counter-example/static/js/2.95addc7e.chunk.js"
+  },
+  {
+    "revision": "c76f3edaa3b5d49e4bc3",
+    "url": "/team-counter-example/static/js/main.39c1f09e.chunk.js"
+  },
+  {
+    "revision": "5d44081da0c9d053c185",
+    "url": "/team-counter-example/static/js/runtime~main.a7810cc0.js"
+  },
+  {
+    "revision": "07699de76ee9f82e480ad403b9e97983",
+    "url": "/team-counter-example/static/media/employee.07699de7.png"
+  },
+  {
+    "revision": "299d95a1855761859728b25274bea1a6",
+    "url": "/team-counter-example/static/media/leader.299d95a1.png"
+  }
+]);
